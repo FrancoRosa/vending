24fd8f26871fc78aed6e628109faf86402fcc6ff
FrancoRosa <km115.franco@gmail.com>
2023-06-10 14:47:46 -0500

    Fit to PCB

--- vending.ino
+++ vending.ino
@@ -1,17 +1,26 @@
 #define led 13
-#define valve 11
+#define valve 2
 #define coin_sensor 10
 #define water_sensor 9
 #define vol_per_pulse 45;
 
-int coin_count = 0;
-int water_count = 0;
-int debt = 0;
+
+long signed coin_count = 0;
+long signed water_count = 0;
+long signed debt = 0;
+
+void valve_on() {
+  digitalWrite(valve, LOW);
+}
+
+void valve_off() {
+  digitalWrite(valve, HIGH);
+}
 
 void coins() {
   coin_count++;
   debt += vol_per_pulse;
-  digitalWrite(valve, HIGH);
+  valve_on();
 }
 
 void water() {
@@ -19,16 +28,19 @@ void water() {
   if (debt > 0)
     debt--;
   else
-    digitalWrite(valve, LOW);
+    valve_off();
 }
 
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(valve, OUTPUT);
   pinMode(coin_sensor, INPUT_PULLUP);
   pinMode(water_sensor, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(coin_sensor), coins, FALLING);
   attachInterrupt(digitalPinToInterrupt(water_sensor), water, FALLING);
+  valve_off();
   Serial.begin(9600);
+  Serial.println("...start");
 }
 
 void loop() {
